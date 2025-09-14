141e160164cdf9a8a12e5d674cae559052417612
Bipin11011 <jayavardhanpudi@gmail.com>
2025-09-14 11:00:32 +0530

    Create 1A.c

---
+++ 1A.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    long long n, m, a;
+    scanf("%lld %lld %lld", &n, &m, &a);
+
+    long long rows = (n + a - 1) / a;
+    long long cols = (m + a - 1) / a;
+
+    printf("%lld\n", rows * cols);
+
+    return 0;
+}
